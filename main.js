4463a92f1503507ce6b4fdad1c96cd7f1e9580c3
Mohammed <135312203+Elsehmawy@users.noreply.github.com>
2023-08-14 18:46:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+let nav = document.querySelector("nav");
+btn.addEventListener("click", function() {
+    nav.classList.add("active");
+    btn.classList.add("active");
+}
+)
